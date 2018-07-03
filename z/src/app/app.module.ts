56dcc4f12584f7e02028e5e1f406768b0f35e24b
kd <karna.dalal@gmail.com>
2018-07-04 00:10:27 +0200

    Signed-off-by: kd <karna.dalal@gmail.com> with router outlets

--- z/src/app/app.module.ts
+++ z/src/app/app.module.ts
@@ -1,12 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { UiNavComponent } from '../ui-nav/ui-nav.component';
 import { PageLoginModule } from '../_shared/page/page-Login/page-Login.module';
 import { UsrModule } from './usr/usr.module';
 
+const appRoutes = [
+    {
+        path: 'usrAuth', 
+        loadChildren: '../app/usr/usr.module#UsrModule'
+    },
+   { 
+       path : '', 
+       redirectTo: '/usrAth', 
+       pathMatch: 'full'
+    },
+   { 
+       path : '**', 
+       component: UiNavComponent
+    }
+];
 
 @NgModule({
    declarations: [
@@ -16,6 +32,7 @@ import { UsrModule } from './usr/usr.module';
    imports: [
       BrowserModule,
       ReactiveFormsModule,
+      RouterModule.forRoot(appRoutes),
       UsrModule,
       PageLoginModule
    ],
--- z/src/app/usr/usr.module.ts
+++ z/src/app/usr/usr.module.ts
@@ -1,19 +1,43 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
-
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
 import  { UsrSigninComponent } from './usr-signin/usr-signin.component';
 import { UsrSignupComponent } from './usr-signup/usr-signup.component';
 import { UsrComponent } from './usr.component';
 import { _utils } from '../../_shared/_utils';
 
+const appRoutes = [
+    { path : 'usrAth', component: UsrComponent},
+    { path : 'usrAth/signin', component: UsrSigninComponent},
+    { path : 'usrAth/signup', component: UsrSignupComponent}
+ ];
 
+ const usrAuthRoutes: Routes = [
+    {
+      path: '',
+      component: UsrComponent,
+      //canActivate: [AuthGuard],
+      children: [
+        {
+          path: '',
+          //canActivateChild: [AuthGuard],
+          children: [
+            { path: 'signin', component: UsrSigninComponent },
+            { path: 'signup', component: UsrSignupComponent },
+            { path: '', component: UsrSigninComponent }
+          ]
+        }
+      ]
+    }
+  ];
 
 @NgModule({
     imports: [
         CommonModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterModule.forChild(usrAuthRoutes)
 //        ,
  //       UsrSigninComponent,
 //        UsrSignupComponent
@@ -26,7 +50,8 @@ import { _utils } from '../../_shared/_utils';
     exports: [
         UsrComponent,
         UsrSigninComponent,
-        UsrSignupComponent
+        UsrSignupComponent,
+        RouterModule
     ],
     providers: [
         _utils
