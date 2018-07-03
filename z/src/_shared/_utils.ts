64acc19518cc6604b7861b862b0b86e1e0112f56
kd <karna.dalal@gmail.com>
2018-07-03 12:33:55 +0200

    Signed-off-by: kd <karna.dalal@gmail.com>

--- z/src/_shared/_utils.ts
+++ z/src/_shared/_utils.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@angular/core';
 import * as CryptoJS from 'crypto-js';
 
 @Injectable()
-export class _utils 
-{
-    getSHA1(_plainText : string)
-    {
-        if(_plainText != '')
+export class _utils {
+    getSHA1(_plainText: string) {
+        if (_plainText !== '') {
         return (CryptoJS.SHA1(_plainText));
+        }
     }
 }
